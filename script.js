84f73349b3604be50b5148cb5c34a88e2c91e85f
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-04 13:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	 if (!str) return [];
+
+  const chunks = [];
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+  return chunks;
 }
 
 // Do not change the code below
